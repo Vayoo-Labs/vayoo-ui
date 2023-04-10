0079edb500e5ec6e8a41ac25b0334525a1af953e
Muhammed Adil <adil@mcubeh.com>
2023-04-10 19:42:10 +0530

    change 4 button trade to 2 button

--- src/components/trade.tsx
+++ src/components/trade.tsx
@@ -401,7 +401,7 @@ const Trade = () => {
   };
 
   return (
-    <div className="w-full max-w-sm py-6 text-white flex flex-col gap-3 border-2 border-gray-300/10 rounded-xl bg-black/50 z-10">
+    <div className="w-full max-w-xs py-6 text-white flex flex-col gap-3 border-2 border-gray-300/10 rounded-xl bg-black/50 z-10">
       <div className="px-6 text-lg text-gray-200">
         Trade {selectedContract?.extraInfo?.short_name}
       </div>
@@ -450,9 +450,7 @@ const Trade = () => {
                 "NIL"}
             </div>
             <div className="text-gray-500 text-xs underline-offset-4">
-              <a href={selectedContract?.extraInfo.oracle_link}>
-                oracle
-              </a>
+              <a href={selectedContract?.extraInfo.oracle_link}>oracle</a>
             </div>
           </div>
           |
@@ -491,11 +489,11 @@ const Trade = () => {
             <div className="flex flex-col gap-4 items-start">
               <div className="flex flex-col items-end text-lg">
                 Amount
-                <div className="mt-5 text-gray-500 text-xs underline-offset-4">
+                <div className="mt-4 text-gray-500 text-xs underline-offset-4">
                   USDC
                 </div>
               </div>
-              <div className="mt-6 w-full flex flex-col items-end text-lg">
+              <div className="mt-4 w-full flex flex-col items-end text-lg">
                 <div className="text-gray-500 text-xs underline-offset-4 text-right">
                   No of contracts
                 </div>
@@ -506,11 +504,11 @@ const Trade = () => {
                 <input
                   value={usdcInputValue}
                   onChange={(e) => onChangeSeconderyUsdValue(e.target.value)}
-                  className="w-full py-3 text-sm px-3 text-gray-100 rounded-l-lg border-2 border-gray-100/10 border-r-0 bg-gray-100/10 focus:outline-none"
+                  className="w-full py-2 text-sm px-3 text-gray-100 rounded-l-lg border-2 border-gray-100/10 border-r-0 bg-gray-100/10 focus:outline-none"
                 />
                 <div
                   onClick={setMaxUsd}
-                  className="flex flex-col py-3 items-center w-32 text-sm text-gray-400 border-2 border-gray-100/30 rounded-r-lg hover:border-gray-200/60 hover:text-white cursor-pointer"
+                  className="flex flex-col py-2 items-center w-32 text-sm text-gray-400 border-2 border-gray-100/30 rounded-r-lg hover:border-gray-200/60 hover:text-white cursor-pointer"
                 >
                   Max
                 </div>
@@ -521,18 +519,18 @@ const Trade = () => {
                   onChange={(e) =>
                     onChangeSeconderyContractValue(e.target.value)
                   }
-                  className="w-full py-3 text-sm px-3 text-gray-100 rounded-l-lg border-2 border-gray-100/10 border-r-0 bg-gray-100/10 focus:outline-none"
+                  className="w-full py-2 text-sm px-3 text-gray-100 rounded-l-lg border-2 border-gray-100/10 border-r-0 bg-gray-100/10 focus:outline-none"
                 />
                 <div
                   onClick={setMaxContract}
-                  className="flex flex-col py-3 items-center w-32 text-sm text-gray-400 border-2 border-gray-100/30 rounded-r-lg hover:border-gray-200/60 hover:text-white cursor-pointer"
+                  className="flex flex-col py-2 items-center w-32 text-sm text-gray-400 border-2 border-gray-100/30 rounded-r-lg hover:border-gray-200/60 hover:text-white cursor-pointer"
                 >
                   Max
                 </div>
               </div>
             </div>
           </div>
-          <p className="my-4 ml-20 px-6 text-red-900 text-sm">{errStr}</p>
+          <p className="mt-4 ml-20 px-6 text-red-900 text-sm">{errStr}</p>
           <div className="px-8 flex items-center justify-between mt-2">
             <p className="text-sm text-gray-500 font-regular font-poppins dark:text-white-900">
               Slippage Tolerance
@@ -547,49 +545,51 @@ const Trade = () => {
             </div>
           </div>
 
-          <div className="mt-4 px-6 mb-1 flex flex-row w-full justify-between gap-3">
-            <div className="w-full flex flex-col justify-between items-center py-1 rounded-xl gap-[0.5px]">
+          <div className="mt-3 px-6 flex flex-row w-full justify-between gap-3">
+            <div className="w-full py-1 rounded-xl">
               <button
                 disabled={
-                  state?.userPosition == UserPosition.Short || !tradeEnable
+                 !tradeEnable
                 }
-                onClick={onClickOpenLong}
-                className="w-full py-2 text-gray-100 bg-green-400/30 rounded-t-xl border-2 border-black hover:border-green-400/60 text-sm disabled:border disabled:border-gray-500/40 disabled:bg-black disabled:text-gray-400"
+                onClick={state?.userPosition == UserPosition.Neutral || state?.userPosition == UserPosition.Mm
+                  ? onClickOpenLong
+                  : state?.userPosition == UserPosition.Long
+                  ? onClickOpenLong
+                  : onClickOpenShort}
+                className={`${state?.userPosition == UserPosition.Neutral || state?.userPosition == UserPosition.Mm
+                  ? "bg-green-400/30 hover:border-green-400/60"
+                  : state?.userPosition == UserPosition.Long
+                  ? "bg-green-400/30 hover:border-green-400/60"
+                  : "bg-red-400/30 hover:border-red-400/60"} w-full py-3 text-gray-100  rounded-lg border-2 border-white/10 text-sm disabled:border disabled:border-gray-500/40 disabled:bg-black disabled:text-gray-400`}
               >
-                Open Long
-              </button>
-              <button
-                disabled={
-                  state?.userPosition == UserPosition.Short ||
-                  state?.userPosition == UserPosition.Neutral ||
-                  !tradeEnable
-                }
-                onClick={onClickCloseLong}
-                className="w-full py-2 text-gray-100 bg-green-400/10 rounded-b-xl border-2 border-black hover:border-green-400/40 text-sm disabled:border disabled:border-gray-500/40 disabled:bg-black disabled:text-gray-400"
-              >
-                Close Long
+                {state?.userPosition == UserPosition.Neutral || state?.userPosition == UserPosition.Mm
+                  ? "Long"
+                  : state?.userPosition == UserPosition.Long
+                  ? "Long"
+                  : "Short"}
               </button>
             </div>
             <div className="w-full flex flex-col justify-between items-center py-1 border-green-100/60 rounded-xl">
               <button
                 disabled={
-                  state?.userPosition == UserPosition.Long || !tradeEnable
-                }
-                onClick={onClickOpenShort}
-                className="w-full py-2 text-gray-100 bg-red-400/30 rounded-t-xl border-2 border-black hover:border-red-400/60 text-sm disabled:border disabled:border-gray-500/40 disabled:bg-black disabled:text-gray-400"
-              >
-                Open Short
-              </button>
-              <button
-                disabled={
-                  state?.userPosition == UserPosition.Long ||
-                  state?.userPosition == UserPosition.Neutral ||
                   !tradeEnable
                 }
-                onClick={onClickCloseShort}
-                className="w-full py-2 text-gray-100 bg-red-400/10 rounded-b-xl border-2 border-black hover:border-red-400/40 text-sm disabled:border disabled:border-gray-500/40 disabled:bg-black disabled:text-gray-400"
+                onClick={state?.userPosition == UserPosition.Neutral || state?.userPosition == UserPosition.Mm
+                  ? onClickOpenShort
+                  : state?.userPosition == UserPosition.Long
+                  ? onClickCloseLong
+                  : onClickCloseShort}
+                className={`${state?.userPosition == UserPosition.Neutral || state?.userPosition == UserPosition.Mm
+                  ? "bg-red-400/30 hover:border-red-400/60"
+                  : state?.userPosition == UserPosition.Long
+                  ? "bg-green-400/10 hover:border-green-400/40"
+                  : "bg-red-400/10 hover:border-red-400/40"} w-full py-3 text-gray-100  rounded-lg border-2 border-white/10 text-sm disabled:border disabled:border-gray-500/40 disabled:bg-black disabled:text-gray-400`}
               >
-                Close Short
+                {state?.userPosition == UserPosition.Neutral || state?.userPosition == UserPosition.Mm
+                  ? "Short"
+                  : state?.userPosition == UserPosition.Long
+                  ? "Close Long"
+                  : "Close Short"}
               </button>
             </div>
           </div>
